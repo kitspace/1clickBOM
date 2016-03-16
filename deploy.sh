579073c66c22b15fe87ba5b35428be811aa907de
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-03-16 19:00:14 +0000

    Make deploy.sh work for actually pushing

--- deploy.sh
+++ deploy.sh
@@ -11,10 +11,10 @@ else
     sudo apt-get install pandoc
     git clone https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} pages
     cd pages
+    git config user.name "Travis CI"
+    git config user.email "travisCI@monostable.co.uk"
     git checkout gh-pages
     make || exit 1
     make commit || exit 0 # allowed to fail if nothing to commit
-    git config user.name "Travis CI"
-    git config user.email "travisCI@monostable.co.uk"
     git push origin gh-pages
 fi
