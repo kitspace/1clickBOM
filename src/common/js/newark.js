1d209b648e0d3216822b2b7f1775c6e9e7fd3768
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-02-21 21:40:09 +0000

    Make farnell error detection make in all supported languages

--- src/common/js/newark.js
+++ src/common/js/newark.js
@@ -296,84 +296,120 @@ class Newark extends RetailerInterface {
                                     for (let j = 0; j < lines.length; j++) {
                                         var line = lines[j]
                                         const regex = new RegExp(line.part, 'g')
-                                        const result = regex.exec(
+                                        const result = regex.test(
                                             form_errors.innerHTML
                                         )
-                                        if (result != null) {
-                                            // TODO: make this work for all languages
-                                            // ignore 'has a pack size of ...' errors
-                                            // XXX i don't think we actually ever receive these here
+                                        if (result) {
                                             const p = line.part
-                                            const regex_pack = new RegExp(
-                                                //english
-                                                `(${p} has a pack)` +
-                                                    //spanish
-                                                    `|(${p} tiene \\d+ artículos)` +
-                                                    //german
-                                                    `|(${p} hat eine Verpackungsgröße von)` +
-                                                    //dutch
-                                                    `|(${p}  heeft een pakketgrootte van)` +
-                                                    //french
-                                                    `|(${p} est disponible par groupe de)` +
-                                                    //bulgarian
-                                                    `|(${p} има количество на опаковката от)` +
-                                                    //czech
-                                                    `|(${p} obsahuje v balení)` +
-                                                    //danish
-                                                    `|(${p} har en pakkestørrelse på)` +
-                                                    //estonian
-                                                    `|(${p} pakend sisaldab)` +
-                                                    //finish
-                                                    `|(${p} pakkauskoko on)` +
-                                                    //hungarian
-                                                    `|(${p} termék csomagmérete)`
-                                            )
-                                            const result_pack = regex_pack.exec(
-                                                form_errors.innerHTML
-                                            )
-                                            // ignore 'needs to be order in multiples ...' errors
+                                            // ignore 'needs to be order in multiples ...' and similar errors
                                             const regex_multiples = new RegExp(
                                                 //english
                                                 `(${p} can only be ordered to a minimum)` +
                                                     `|(${p} needs to be ordered in multiples of)` +
+                                                    `|(${p} has a pack)` +
                                                     //spanish
                                                     `|(${p} solo se puede pedir en cantidades mínimas de)` +
                                                     `|(${p} tiene que pedirse en múltiplos de)` +
+                                                    `|(${p} tiene \\d+ artículos)` +
                                                     //german
                                                     `|(${p} muss die Mindestmenge von)` +
                                                     `|(${p} muss in Staffelungen von)` +
+                                                    `|(${p} hat eine Verpackungsgröße von)` +
                                                     //dutch
                                                     `|(${p} moet worden besteld in veelvouden van)` +
                                                     `|(${p} alleen worden besteld met een minimaal aantal van)` +
+                                                    `|(${p} heeft een pakketgrootte van)` +
                                                     //french
                                                     `|(${p} doit être commandé par multiple de)` +
                                                     `|(${p} peut être commandé uniquement avec une quantité minimale)` +
+                                                    `|(${p} est disponible par groupe de)` +
                                                     //bulgarian
                                                     `|(${p} трябва да се поръчва в множество от)` +
                                                     `|(${p} може да се поръчва само при минимално количество от)` +
+                                                    `|(${p} има количество на опаковката от)` +
                                                     //czech
                                                     `|(${p} je nutné objednat)` +
                                                     `|(${p} lze objednat pouze)` +
+                                                    `|(${p} obsahuje v balení)` +
                                                     //danish
                                                     `|(Minimumsantallet ved bestilling af produkt ${p})` +
                                                     `|(${p} skal bestilles i antal deleligt med)` +
+                                                    `|(${p} har en pakkestørrelse på)` +
                                                     //estonian
                                                     `|(${p} tuleb tellida)` +
                                                     `|(${p} minimaalne tellitav kogus on)` +
+                                                    `|(${p} pakend sisaldab)` +
                                                     //finish
                                                     `|(${p} vähimmäistilausmäärä on)` +
                                                     `|(${p} on tilattava)` +
+                                                    `|(${p} pakkauskoko on)` +
                                                     //hungarian
                                                     `|(${p} esetén a megrendelt mennyiség csak)` +
-                                                    `|(${p} termék csak)`
+                                                    `|(${p} termék csak)` +
+                                                    `|(${p} termék csomagmérete)` +
+                                                    //italian
+                                                    `|(${p} deve essere ordinato in multipli di)` +
+                                                    `|(${p} ?può essere ordinato solo nella quantità minima pari a)` +
+                                                    `|(della confezione del prodotto ${p} è di)` +
+                                                    //latvian
+                                                    `|(${p} pasūtījuma daudzumam ir jādalās ar)` +
+                                                    `|(${p} ?var pasūtīt tikai minimālo daudzumu)` +
+                                                    `|(Produkta ${p} iepakojumā ir)` +
+                                                    //lithuanian
+                                                    `|(${p} galima užsakyti ne mažiau kaip)` +
+                                                    `|(${p} turi būti užsakomas kartotiniais po)` +
+                                                    `|(${p} yra supakuotas po)` +
+                                                    //norwegian
+                                                    `|(${p} kan bare bestilles i antall delelig med)` +
+                                                    `|(${p} ?kan bare bestilles i minimumsantall på)` +
+                                                    `|(${p} leveres i pakker på)` +
+                                                    //polish
+                                                    `|(${p} należy zamawiać w ilości stanowiącej wielokrotność liczby)` +
+                                                    `|(${p} minimalna ilość zamówienia wynosi)` +
+                                                    `|(${p} jest sprzedawany w ilości)` +
+                                                    //portugese (only 2 kinds it seems)
+                                                    `|(${p} só pode ser encomendado numa quantidade mínima de)` +
+                                                    `|(${p} tem um tamanho de embalagem de)` +
+                                                    //romanian
+                                                    `|(${p} ?poate fi comandat numai într-o cantitate minimă de)` +
+                                                    `|(${p} trebuie comandat în cantităţi multiple de)` +
+                                                    `|(${p} este livrat în pachete de)` +
+                                                    //russian
+                                                    `|(${p} заказывается в количестве, кратном)` +
+                                                    `|(Минимальное количество для заказа ${p})` +
+                                                    `|(${p} поставляется в упаковке, содержащей)` +
+                                                    //slovak
+                                                    `|(Produkt ${p} je možné objednať len)` +
+                                                    `|(${p} ?-? Ďakujeme, že ste si objednali produkt z našej rozšírenej ponuky)` +
+                                                    `|(Produkt ${p} je balený po)` +
+                                                    //slovenian
+                                                    `|(${p} mora biti večkratnik števila)` +
+                                                    `|(Najmanjša količina za naročilo izdelka ${p})` +
+                                                    `|(${p} je pakiran po)` +
+                                                    //swedish
+                                                    `|(${p} måste beställas i multiplar om)` +
+                                                    `|(${p} ?kan endast beställas till ett minimiantal av)` +
+                                                    `|(${p} levereras i paket om)` +
+                                                    //turkish
+                                                    `|(${p} ürününün \\d+ ve katları olarak)` +
+                                                    `|(${p} ürününün ambalaj boyutu)` +
+                                                    //chinese
+                                                    `|(产品 ${p} 的最小包装为)` +
+                                                    `|(${p} 的最小包装为)` +
+                                                    //korean
+                                                    `|(${p} 은(는) 5의 배수로 주문해야 합니다. 9에서)` +
+                                                    `|(제품 ${p}은\\(는)` +
+                                                    //taiwanese
+                                                    `|(${p} 的最低訂購數量是)` +
+                                                    `|(產品 ${p} 以 \\d+ 個項目為)` +
+                                                    //thai
+                                                    `|(ต้องสั่งซื้อผลิตภัณฑ์ ${p} จำนวน)` +
+                                                    `|(ผลิตภัณฑ์ ${p} มีขนาดบรรจุ)`
                                             )
-                                            const result_multiples = regex_multiples.exec(
+                                            const is_multiple_warning = regex_multiples.test(
                                                 form_errors.innerHTML
                                             )
-                                            if (
-                                                result_pack == null &&
-                                                result_multiples == null
-                                            ) {
+                                            if (!is_multiple_warning) {
                                                 fails.push(line)
                                             }
                                         }
